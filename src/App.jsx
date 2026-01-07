56323c200ae394ba48f481e2ac59f7d664b14522
GmatiAyoub <gmtayoyoub15@gmail.com>
2026-01-07 14:11:02 +0100

    useffect exs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-export default function App(){
-  const [text,setText]=useState('');
-  return(
-    <>
-    <input type="text" onChange={(e)=> setText(e.target.value)} />
-    <p>{text}</p>
-    </>
-  )
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Compteur : ${count}`;
+  }, [count]);
+
+  return <button onClick={() => setCount(count + 1)}>+</button>;
 }
